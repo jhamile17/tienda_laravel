786fc0b4ab00fc79e56d8d4c4ae860b5a6f4b2a9
jhamile17 <https://github.com/jhamile17/proyectocoolify.git>
2026-06-16 23:49:57 -0500

    agregue controlador

--- app/Http/Controllers/ChatbotController.php
+++ app/Http/Controllers/ChatbotController.php
@@ -3,8 +3,44 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class ChatbotController extends Controller
 {
-    //
+    public function send(Request $request)
+    {
+        try {
+
+            $message = $request->message;
+
+            $apiKey = env('GEMINI_API_KEY');
+
+            $response = Http::post(
+                "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={$apiKey}",
+                [
+                    "contents" => [
+                        [
+                            "parts" => [
+                                [
+                                    "text" => $message
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            );
+
+            return response()->json([
+                'status' => $response->status(),
+                'body' => $response->json()
+            ]);
+
+        } catch (\Exception $e) {
+
+            return response()->json([
+                'error' => $e->getMessage()
+            ], 500);
+
+        }
+    }
 }
