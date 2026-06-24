1c903db0325f4734da13d0ea020212563454cee2
jhamile17 <https://github.com/jhamile17/proyectocoolify.git>
2026-06-24 13:23:43 -0500

    Ignorar vistas compiladas de Laravel

--- storage/framework/views/c4f8b7e688f4f08b5f9eed769dbb3e19.php
+++
@@ -1,5 +0,0 @@
-<a href="https://wa.me/51999999999?text=Hola%20quiero%20hacer%20un%20pedido"
-   target="_blank"
-   class="position-fixed bottom-0 end-0 m-4 btn btn-success rounded-circle shadow">
-    <i class="bi bi-whatsapp fs-4"></i>
-</a><?php /**PATH D:\Pagina-web-\resources\views/components/ecommerce/whatsapp.blade.php ENDPATH**/ ?>
--- storage/framework/views/ca0be494918bcbb2b00bbc967bab9c75.php
+++
@@ -1,5 +0,0 @@
-<footer class="border-top py-4 mt-5 bg-white">
-    <div class="container text-center text-muted small">
-        © <?php echo e(date('Y')); ?> PROCAFES. Todos los derechos reservados.
-    </div>
-</footer><?php /**PATH D:\Pagina-web-\resources\views/components/layout/footer.blade.php ENDPATH**/ ?>
